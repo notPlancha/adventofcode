a92e5b6e15f38a4f63a095e05671ff8e247d7371
notPlancha <andre.plancha@hotmail.com>
2024-12-21 21:09:26 +0000

    part1

--- 2024/day13/part1.py
+++ 2024/day13/part1.py
@@ -1,13 +1,23 @@
+import math
 from pathlib import Path
 from dataclasses import dataclass
-from collections import namedtuple
 import re
+from typing import NamedTuple
+from icecream import ic
+import sympy
+from pulp import *
+
+ic.disable()
 
 re_b = re.compile(r"Button [AB]: X\+(\d+), Y\+(\d+)")
 re_p = re.compile(r"Prize: X=(\d+), Y=(\d+)")
 
-Button = namedtuple("Button", "x y")
-Prize = namedtuple("Prize", "x y")
+class Button(NamedTuple):
+  x: int
+  y: int
+class Prize(NamedTuple):
+  x: int
+  y: int
 
 
 @dataclass
@@ -26,10 +36,35 @@ class Machine:
   prize: Prize
 
 
-def load_data() -> list[Machine]:
-  machines = [section.splitlines() for section in Path("input.txt").read_text().split("\n\n")]
+def load_data(file_name) -> list[Machine]:
+  machines = [section.splitlines() for section in Path(file_name).read_text().split("\n\n")]
   return [Machine.from_list(i) for i in machines]
 
 if __name__ == '__main__':
-  machines = load_data()
+  machines = load_data("input.txt")
+  # to test with 1
+  out = 0
+  for machine in machines:
+    A, B, p = machine.A, machine.B, machine.prize
+    A: Button
+    B: Button
+    p: Prize
 
+
+    # equation is AX*na + BX*nb = px
+    # obj: lowerst tokens
+    # tokens = na*3 + nb * 1
+    na = LpVariable("na", 1, cat=LpInteger)
+    nb = LpVariable("nb", 1, cat=LpInteger)
+    prob = LpProblem("part1", LpMinimize)
+    prob += 3*na + 1*nb
+    prob += A.x*na + B.x*nb == p.x
+    prob += A.y*na + B.y*nb == p.y
+    status = prob.solve(PULP_CBC_CMD(msg=False))
+    ic(prob)
+    ic(prob.objective.value())
+    ic(LpStatus[status], status)
+    if status == 1: # Optimal found
+      out += prob.objective.value()
+
+  print(out)
