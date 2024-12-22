a51660170b1a2ad459864e1eb9e5b0f2a83c011b
notPlancha <andre.plancha@hotmail.com>
2024-12-22 21:52:56 +0000

    graph now correctly does what I want

--- 2024/day11/example_tree.py
+++ 2024/day11/example_tree.py
@@ -1,12 +1,26 @@
-from collections import deque
+from collections import deque, defaultdict
 from itertools import pairwise
 from pathlib import Path
 import networkx as nx
 from networkx.drawing.nx_agraph import to_agraph
 from pyprojroot.here import here
 
+import matplotlib.pyplot as plt
+
+
 def node_on_graph(g, node):
-  return str(node) in g
+  return node in g
+
+
+last_of_level: dict[int, int] = defaultdict(lambda: -1)
+
+
+def add_node(g, node, y_level):
+  if node not in g:
+    last_of_level[y_level] += 1
+    x = last_of_level[y_level]
+    g.add_node(node, pos=(x, y_level))
+    # add node to the graph at the given y_level and a new x, right to the previous node
 
 
 file_name = here("2024/day11/test.txt")
@@ -17,10 +31,8 @@ stones.extend(map(int, Path(file_name).read_text().split(" ")))
 
 # region add root stones
 for stone in stones:
-  g.add_node(str(stone))
-  # with g.subgraph() as s:
-  #   s.graph["rank"] = "min"
-  #   s.add_node(str(stone))
+  add_node(g, stone, 0)
+
 # endregion
 
 # test node_on_graph
@@ -33,45 +45,32 @@ for blinked in range(6):
   for i in stones:
     if i == 0:
       new_stones.append(1)
-      g.add_node("1")
-      g.add_edge("0", "1")
+      add_node(g, 1, blinked + 1)
+      g.add_edge(0, 1)
     elif len(str_i := str(i)) % 2 == 0:
       mid_point = len(str_i) // 2
       new_stones.append(int(str_i[:mid_point]))
-      g.add_node(str(int(str_i[:mid_point])))
-      g.add_edge(str(i), str(int(str_i[:mid_point])))
-      g.add_edge(str(i), str(int(str_i[:mid_point])))
+      add_node(g, int(str_i[:mid_point]), blinked + 1)
+      g.add_edge(i, int(str_i[:mid_point]))
+      g.add_edge(i, int(str_i[:mid_point]))
       new_stones.append(int(str_i[mid_point:]))
-      g.add_node(str(int(str_i[mid_point:])))
-      g.add_edge(str(i), str(int(str_i[mid_point:])))
+      add_node(g, int(str_i[mid_point:]), blinked + 1)
+      g.add_edge(i, int(str_i[mid_point:]))
     else:
       new_stone = i * 2024
       new_stones.append(new_stone)
-      g.add_node(f"{new_stone}")
-      g.add_edge(f"{i}", f"{new_stone}")
-  # endregion
-  # region add newly created nodes as same rank
-  # with g.subgraph() as s:
-  #   s.graph["rank"] = "same"
-  #   for stone in new_stones:
-  #     if not node_on_graph(g, stone):
-  #       s.add_node(f"{stone}")
+      add_node(g, new_stone, blinked + 1)
+      g.add_edge(i, new_stone)
   # endregion
   stones = new_stones
 
-# region define root nodes as rank min
-for stone in stones:
-  g.add_node(stone)
-  # with g.subgraph() as s:
-  #   s.graph["rank"] = "min"
-  #   s.add_node(stone)
-# endregion
-import matplotlib.pyplot as plt
+# get the position of each node
+pos = nx.get_node_attributes(g, "pos")
+print(pos)
 
 # Visualize the graph
-graph_layout = nx.nx_agraph.graphviz_layout(g, prog="dot")
 graph_fig = plt.figure(figsize=(10, 10))
-nx.draw(g, pos=graph_layout, with_labels=True, node_size=1000, font_size=10)
+nx.draw(g, pos=pos, with_labels=True, node_size=1000, font_size=10)
 plt.title("Graph Visualization")
 plt.savefig(here("2024/day11/graph.pdf"))
-plt.close()
+plt.show()
